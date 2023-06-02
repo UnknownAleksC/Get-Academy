3f708b57d46b2cef62b2a599bc3c16bbef397f05
UnknownBear <aleksanderca@getacademy.no>
2023-06-02 14:47:55 +0200

    Arbeid Uke 5

---
+++ C# Arbeid Organisert/Uke 5/ShoppingCart-V2/Shopping Cart/CartItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_Cart
+{
+    public class CartItem
+    {
+        private Product _product;
+        private int _amount;
+
+        public CartItem(Product product, int amount)
+        {
+            _product = product;
+            _amount = amount;
+        }
+
+        public void AddAmount(int amount)
+        {
+            _amount += amount;
+        }
+
+        public void ShowOrderLinePrice()
+        {
+            _product.CalculateOrderLinePrice(_amount);
+        }
+
+        public string GetProductName()
+        {
+            return _product.GetName();
+        }
+    }
+}
---
+++ C# Arbeid Organisert/Uke 5/ShoppingCart-V2/Shopping Cart/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_Cart
+{
+    public class Product
+    {
+        private string _name;
+        private int _price;
+
+        public Product(string productName, int productPrice)
+        {
+            _name = productName;
+            _price = productPrice;
+        }
+
+        public void CalculateOrderLinePrice(int amount)
+        {
+            var orderLinePrice = _price * amount;
+            Console.WriteLine($"{amount}stk {_name}, {_price}kr per = {orderLinePrice}kr");
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
