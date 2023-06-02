60c637e495ad076bf2cf583c9fc88dee3648a165
UnknownBear <aleksanderca@getacademy.no>
2023-06-02 14:46:58 +0200

    Uke 5 Arbeid

---
+++ C# Arbeid Organisert/Uke 5/Codealong 31-09-2023/Coin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codealong_31_09_2023
+{
+    internal class Coin
+    {
+        private int Value;
+        private int Count;
+
+        public Coin(int value, int count)
+        {
+            Value = value;
+            Count = count;
+        }
+
+        public int CalculateTotalCoinValue()
+        {
+            return Value * Count;
+        }
+    }
+}
---
+++ C# Arbeid/Startliste/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Startliste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var startList = File.ReadAllLines("startlist.csv");
+            var registrationList = new List<Registration>();
+            var clubList = new List<Club>();
+
+            foreach (var line in startList)
+            {
+                //line.Trim(new Char[] { ',', '\"' });
+                var lineSeperated = line.Split(",");
+                var newRegistration = new Registration(
+                    lineSeperated[0].Trim(new Char[] { ' ', ',', '\"' }),
+                    lineSeperated[1],
+                    lineSeperated[2],
+                    lineSeperated[3],
+                    lineSeperated[4],
+                    lineSeperated[5]
+                );
+                registrationList.Add(newRegistration);
+                if (clubList.Find(x => x.GetName() == lineSeperated[2]) == null)
+                    clubList.Add(new Club(lineSeperated[2]));
+            }
+
+            foreach (var club in clubList)
+            {
+                club.AddRegistration(registrationList);
+            }
+        }
+    }
+}
